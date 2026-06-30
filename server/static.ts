4b45c014a3e41764593bf4627b6737f501702ae5
quinnco23 <59463792+quinnco23@users.noreply.github.com>
2026-06-30 14:09:38 -0700

    Update static.ts
    static

--- server/static.ts
+++ server/static.ts
@@ -6,7 +6,7 @@ export function serveStatic(app: express.Express) {
 
   app.use(express.static(distPath));
 
-  app.get("*", (_req, res) => {
+  app.use((_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
