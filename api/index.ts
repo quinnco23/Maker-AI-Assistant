dd2e490ad8ebf833386199ee9585e89626c9f84c
quinnco23 <59463792+quinnco23@users.noreply.github.com>
2026-06-29 23:07:55 -0700

    js index
    palease

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,8 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import express from "express";
 import { createServer } from "http";
 
-import { setupAuth } from "../server/auth.ts";
-import { registerRoutes } from "../server/routes.ts";
+import { setupAuth } from "../server/auth.js";
+import { registerRoutes } from "../server/routes.js";
 
 let appPromise: Promise<express.Express> | null = null;
 
